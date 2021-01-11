1b55175c47cfe30c28a0ace933846dfcd28f43ea
hzw456 <453967724@qq.com>
2021-01-11 15:37:51 +0800

    add google s2 func

--- googleS2.go
+++ googleS2.go
@@ -5,7 +5,19 @@ import (
 	"github.com/golang/geo/s2"
 )
 
-func S2GetCellID(p Point) int64 {
-	cellID := s2.CellIDFromLatLng(s2.LatLng{Lat: s1.Angle(p.X), Lng: s1.Angle(p.Y)})
-	return int64(cellID)
+func S2GetCellID(p Point, level int, srid SRID) int64 {
+	if srid == SRID_WGS84_GPS {
+		cellID := s2.CellIDFromLatLng(s2.LatLng{Lat: s1.Angle(p.X), Lng: s1.Angle(p.Y)}).Parent(level)
+		return int64(cellID)
+	}
+	return -1
+}
+
+func S2GetCenter(cellID int64, level int, srid SRID) *Point {
+	cellIDS2 := s2.CellID(cellID)
+	if srid == SRID_WGS84_GPS {
+		coord := cellIDS2.LatLng()
+		return &Point{float64(coord.Lat), float64(coord.Lng)}
+	}
+	return nil
 }
