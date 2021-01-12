8a48fa7a12a0aa48e6be2a9b3332b54ff31e7612
hzw456 <453967724@qq.com>
2021-01-12 12:05:04 +0800

    fix int64 overflow

--- googleS2.go
+++ googleS2.go
@@ -5,15 +5,15 @@ import (
 	"github.com/golang/geo/s2"
 )
 
-func S2GetCellID(p Point, level int, srid SRID) int64 {
+func S2GetCellID(p Point, level int, srid SRID) uint64 {
 	if srid == SRID_WGS84_GPS {
 		cellID := s2.CellIDFromLatLng(s2.LatLng{Lat: s1.Angle(p.X), Lng: s1.Angle(p.Y)}).Parent(level)
-		return int64(cellID)
+		return uint64(cellID)
 	}
 	return -1
 }
 
-func S2GetCenter(cellID int64, level int, srid SRID) *Point {
+func S2GetCenter(cellID uint64, level int, srid SRID) *Point {
 	cellIDS2 := s2.CellID(cellID)
 	if srid == SRID_WGS84_GPS {
 		coord := cellIDS2.LatLng()
