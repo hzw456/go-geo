956b70677a047ce7b71eaaea8b0c3bed9210ccd0
hzw456 <453967724@qq.com>
2021-05-24 12:13:45 +0800

    update coord distance

--- tests/grid_test.go
+++ tests/grid_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestS2(t *testing.T) {
-	cellID := geo.S2GetCellID(geo.Point{23.07293325812827, 113.37101949678055}, 13, geo.SRID_WGS84_GPS)
-	coord := geo.S2GetCenter(cellID, geo.SRID_WGS84_GPS)
+	// cellID := geo.S2GetCellID(geo.Point{23.07293325812827, 113.37101949678055}, 13, geo.SRID_WGS84_GPS)
+	coord := geo.S2GetCenter(11990234155561517056, geo.SRID_WGS84_GPS)
 	t.Log(coord)
 }
