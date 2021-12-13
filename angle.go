7ca6da6e4629b53652ad827065b83162bed7ad99
hzw456 <453967724@qq.com>
2021-12-13 19:17:45 +0800

    fix lat lng bug

--- angle.go
+++ angle.go
@@ -8,10 +8,11 @@ func GetAzimuth(p1, p2 Point, srid SRID) float64 {
 	if srid != SRID_WGS84_GPS {
 		return 0
 	}
-	lat1_rad := p1.X * math.Pi / 180
-	lat2_rad := p2.X * math.Pi / 180
-	lon1_rad := p1.Y * math.Pi / 180
-	lon2_rad := p2.Y * math.Pi / 180
+
+	lon1_rad := p1.X * math.Pi / 180
+	lon2_rad := p2.X * math.Pi / 180
+	lat1_rad := p1.Y * math.Pi / 180
+	lat2_rad := p2.Y * math.Pi / 180
 
 	y := math.Sin(lon2_rad-lon1_rad) * math.Cos(lat2_rad)
 	x := math.Cos(lat1_rad)*math.Sin(lat2_rad) - math.Sin(lat1_rad)*math.Cos(lat2_rad)*math.Cos(lon2_rad-lon1_rad)
--- point.go
+++ point.go
@@ -42,19 +42,19 @@ func (p1 Point) Equal(p2 Point) bool {
 
 //HaverSin
 func CoordDistance(p1, p2 Point) float64 {
-	lat1 := p1.X * math.Pi / 180.0
-	lng1 := p1.Y * math.Pi / 180.0
-	lat2 := p2.X * math.Pi / 180.0
-	lng2 := p2.Y * math.Pi / 180.0
+	lat1 := p1.Y * math.Pi / 180.0
+	lng1 := p1.X * math.Pi / 180.0
+	lat2 := p2.Y * math.Pi / 180.0
+	lng2 := p2.X * math.Pi / 180.0
 	a := math.Pow(math.Sin((lat2-lat1)/2), 2) + math.Cos(lat1)*math.Cos(lat2)*math.Pow(math.Sin((lng2-lng1)/2), 2)
 	return 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a)) * EARTHRADIUSMI
 }
 
 func CoordGreateCircle(p1, p2 Point) float64 {
-	lat1 := p1.X * math.Pi / 180.0
-	lng1 := p1.Y * math.Pi / 180.0
-	lat2 := p2.X * math.Pi / 180.0
-	lng2 := p2.Y * math.Pi / 180.0
+	lat1 := p1.Y * math.Pi / 180.0
+	lng1 := p1.X * math.Pi / 180.0
+	lat2 := p2.Y * math.Pi / 180.0
+	lng2 := p2.X * math.Pi / 180.0
 	return math.Acos(math.Sin(lat1)*math.Sin(lat2)+math.Cos(lat1)*math.Cos(lat2)*math.Cos(lng2-lng1)) * EARTHRADIUSMI
 }
 
