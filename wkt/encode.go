1dc61381d434d59aa5437dcdef18375b615a7e55
luanchao <luanchao@baidu.com>
2022-08-12 10:48:35 +0800

    adapte for polygonz

--- wkt/encode.go
+++ wkt/encode.go
@@ -101,27 +101,27 @@ func Encode(geom geo.Geometry) string {
 	case geo.Point:
 		return fmt.Sprintf("POINT(%s)", GeometryToWktString(geom.X, geom.Y))
 	case geo.PointZ:
-		return fmt.Sprintf("POINT(%s)", GeometryToWktStringZ(geom.X, geom.Y, geom.Z))
+		return fmt.Sprintf("POINT Z (%s)", GeometryToWktStringZ(geom.X, geom.Y, geom.Z))
 	case geo.MultiPoint:
 		return fmt.Sprintf("MULTIPOINT%s", PathToWktString(geom))
 	case geo.MultiPointZ:
-		return fmt.Sprintf("MULTIPOINT%s", PathZToWktString(geom))
+		return fmt.Sprintf("MULTIPOINT Z %s", PathZToWktString(geom))
 	case geo.LineString:
 		return fmt.Sprintf("LINESTRING%s", PathToWktString(geom))
 	case geo.LineStringZ:
-		return fmt.Sprintf("LINESTRING%s", PathZToWktString(geom))
+		return fmt.Sprintf("LINESTRING Z %s", PathZToWktString(geom))
 	case geo.MultiLineString:
 		return fmt.Sprintf("MULTILINESTRING%s", PolygonToWktString(geom))
 	case geo.MultiLineStringZ:
-		return fmt.Sprintf("MULTILINESTRING%s", PolygonZToWktString(geom))
+		return fmt.Sprintf("MULTILINESTRING Z %s", PolygonZToWktString(geom))
 	case geo.Polygon:
 		return fmt.Sprintf("POLYGON%s", PolygonToWktString2(geom))
 	case geo.PolygonZ:
-		return fmt.Sprintf("POLYGON%s", PolygonZToWktString2(geom))
+		return fmt.Sprintf("POLYGON Z %s", PolygonZToWktString2(geom))
 	case geo.MultiPolygon:
 		return fmt.Sprintf("MULTIPOLYGON%s", MultiPolygonToWktString(geom))
 	case geo.MultiPolygonZ:
-		return fmt.Sprintf("MULTIPOLYGON%s", MultiPolygonZToWktString(geom))
+		return fmt.Sprintf("MULTIPOLYGON Z %s", MultiPolygonZToWktString(geom))
 	default:
 		return ""
 	}
